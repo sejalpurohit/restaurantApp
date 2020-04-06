390712eadcb8973d5cfa8fcac1c359e6bc32133b
sejalpurohit <49385581+sejalpurohit@users.noreply.github.com>
2020-04-06 19:01:27 +0530

    Update Franchise.ts

--- Franchise.ts
+++ Franchise.ts
@@ -3,8 +3,8 @@ import { Menu } from "./Restaurant";
 
 export class Franchise{
 
-    address:string;
     name:string;
+    address:string;
     menuList:object[]
 
     constructor(name:string,address:string,menuList:object[]){
@@ -17,24 +17,25 @@ export class Franchise{
         return `${this.name} is located at ${this.address}`;
     }    
 
-    availability(time:number){
-        let nameofItem: string[];
+    availability(time:number):string[]{
+        let nameofItem: string[]=[];
         let name:string;
-        console.log('Available Menu ');
+       
         for(let elements of menuList){  
                let  startTime = +Object.keys(elements).filter((key)=> key==='start_time').map(key => elements[key]);
                 let endTime = +Object.keys(elements).filter((key)=> key==='end_time').map(key => elements[key]);           
         
                 if(time >= startTime && time <= endTime){
                     name = Object.keys(elements).filter((key)=> key==='name').map(key => elements[key]).toString();
-                    console.log(name);
+                    nameofItem.push(name);
                                                   
                  }
         }
+        return (nameofItem);
     }
 }
 
-let menuList:object[] = [ 
+export let menuList:object[] = [ 
     {name:'brunch',item:brunch, start_time:11, end_time:16},
     {name:'early_birds', item:early_bird , start_time:15, end_time:18},
     {name:'dinner', item:dinner, start_time:15, end_time:23},
@@ -45,12 +46,13 @@ let menuList:object[] = [
 
 export let flagShip =new Franchise('flagship_store','123 West Street Road',menuList);
 console.log(flagShip.stores());
-flagShip.availability(17);
+console.log('Available Menu '+flagShip.availability(17));
 
 export let installment = new Franchise('new_installment','456 East Street Road',menuList);
 console.log(installment.stores());
-installment.availability(21);
+console.log('Available Menu '+installment.availability(21));
 
 export let healthyBites = new Franchise('HealthyBites','789North Street Road',menuList);
 console.log(healthyBites.stores());
-healthyBites.availability(21);
+console.log('Available Menu '+healthyBites.availability(21));
+
